2f2ffa863016354a0680a031d7a4b6ee38955da1
dannycrews <dancrews@me.com>
2016-08-18 16:12:46 -0400

    Add a button to update name and bind this

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component {
+  buttonClicked() {
+    this.forceUpdate();
+  }
   render() {
-    return <p>{this.props.message}</p>
+    return  <div>
+              <p>Hello, {chance.first()}.</p>
+              <p>You're from {chance.country({ full: true })}.</p>
+              <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+            </div>;
   }
 }
 
