90091f3f8cf5b4f485a9f422311e9234194b506c
dannycrews <dancrews@me.com>
2016-08-18 19:50:34 -0400

    Implement React Router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import Detail from './pages/Detail';
 
 ReactDOM.render(
-  <Detail />,
-  document.getElementById('app')
-  );
+    <Router history={browserHistory}
+      onUpdate={() => window.scrollTo(0, 0)}>
+      <Route path="/" component={ Detail } />
+    </Router>,
+    document.getElementById('app')
+);
